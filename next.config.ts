b981a629cf1659876aaa7cc9b24ae01986329ee3
ChanchalS7 <chsverma7@gmail.com>
2026-06-09 17:32:08 +0400

    deleted build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: isProduction ? "/portfolio" : "",
 };
 
 export default nextConfig;

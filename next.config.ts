3a7f92f00abf7c17b1aa688f39afe70d3c0902b6
ChanchalS7 <chsverma7@gmail.com>
2026-06-09 16:58:44 +0400

    fixed css deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isProduction ? "/portfolio" : "",
 };
 
 export default nextConfig;
